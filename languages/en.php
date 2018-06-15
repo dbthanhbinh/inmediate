926e03cf8c461d818208fc7d24e3e88ae16e54ff
dbthanhbinh <dbthanhbinh@gmail.com>
2018-06-15 09:31:02 +0700

    init PROJECT

---
+++ languages/en.php
@@ -0,0 +1,11 @@
+<?php
+$lang = '{
+    "navbar": {
+        "Home": "Home",
+        "Service": "Service",
+        "Portfolio": "Portfolio",
+        "About": "About",
+        "Client": "Client",
+        "Contact": "Contact"
+    }
+}';
---
+++ languages/vi.php
@@ -0,0 +1,11 @@
+<?php
+$lang = '{
+    "navbar": {
+        "Home": "Trang chủ",
+        "Service": "Dịch vụ",
+        "Portfolio": "Hình ảnh",
+        "About": "Giới thiệu",
+        "Client": "Khách hàng",
+        "Contact": "Liên hệ"
+    }
+}';
